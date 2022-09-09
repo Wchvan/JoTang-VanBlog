4e6762aec926ae54f200ac2a1f0ea5cf516389e9
Van <1027154225@qq.com>
2022-09-09 11:57:57 +0800

    增加批量删除功能和排序功能

--- index.js
+++ index.js
@@ -84,11 +84,18 @@ for(let i = 0;i < SlideshowSelect.length;i++){
 
 var StudyshowPassageList = document.querySelector(".StudyshowPassage");
 var StudyshowSubmitBtn = document.querySelector(".StudyshowSubmitBtn");
+var StudyshowPassageSortBtn = document.querySelector(".icon-paixu")
+var StudyshowPassageDelSomeBtn = document.querySelector(".icon-piliangshanchu")
 var StudyshowPassageItemList = new Array();
-var StudyshowPassageArea = null;
+var StudyshowPassageArea = null; // 右侧展示文章的区域
+var StudyshowPassageDelItemList = new Array(); // 存放批量删除的对象
 
+var StudyshowPassageDelSomeFlag = 0; // 用于判定批量删除两次按下不同的事件
+var StudyshowPassageSortFlag = 0; // 用于排序的判定
 
+// 建立一个对象用于存储学习记录
 function StudyshowPassageObj(topic,content){
+    // 基本属性设置
     this.topic = topic;
     this.content = content;
     this.date = new Date();
@@ -100,37 +107,37 @@ function StudyshowPassageObj(topic,content){
     this.viewContent.className = "StudyshowPassageItem "
     this.viewContent.textContent = this.topic + "  " + this.date.getFullYear() + "/" + this.date.getMonth() +"/" + this.date.getDate() ;
 
+    // 按钮的设置
     this.btn = document.createElement('span');
-    this.btn.className = "iconfont icon-shanchu";
+    this.btn.className =  "iconfont icon-shanchu";
+    this.btn.index = -1; // 用于确定该对象在数组的索引
+    this.btn.Delindex = -1; //用于确定该对象在删除数组的索引
+    this.btn.flag = 0; // 用来判定文章标题旁边删除按钮的变化
+    
     this.viewContent.appendChild(this.btn);
 
-    this.viewContent.firstElementChild.onclick = function(){
-        this.parentElement.onclick = function(){
-            return false;
-        }
-        if(StudyshowPassageArea){
-            StudyshowPassageArea.display = 'none';
-            for(var i = 1;i < CenterItems.length;i++){
-                CenterItems[i].style.display = 'block';
-            }
-        }
-        this.parentElement.remove()
-    }
-
-    StudyshowPassageList.append(this.viewContent);
+    this.viewContent.firstElementChild.onclick = StudyshowPassageObjDelBtn;
+    StudyshowPassageList.appendChild(this.viewContent);
 }
 
 // 每次表单提交后的事件
 StudyshowSubmitBtn.onclick = function(){
+    // 创立学习记录
     var StudyshowPassageItem = new StudyshowPassageObj(document.StudyshowSubmit.topic.value,document.StudyshowSubmit.content.value);
+    // 存进学习记录的数组
     StudyshowPassageItemList.push(StudyshowPassageItem);
+    // 获取当前记录在数组中的索引值
     StudyshowPassageItem.viewContent.index = StudyshowPassageItemList.length - 1;
+    StudyshowPassageItem.btn.index = StudyshowPassageItem.viewContent.index;
     StudyshowPassageItem.viewContent.onclick = StudyshowPassageItemClick;
 }
 
 
 // 文章标题点击的事件
 function StudyshowPassageItemClick(){
+
+    
+    // 把右侧区域隐藏除了头部 
     for(var i = 1;i < CenterItems.length;i++){
         CenterItems[i].style.display = 'none';
         if(StudyshowPassageArea){
@@ -184,4 +191,83 @@ function StudyshowPassageItemClick(){
         }
         StudyshowPassageItemSelected.viewContent.remove();
     }
+}
+
+// 文章旁边删除按钮的事件
+function StudyshowPassageObjDelBtn(){
+    this.parentElement.onclick = function(){
+        return false;
+    }
+    if(this.flag == 0){
+        if(StudyshowPassageArea){
+            StudyshowPassageArea.display = 'none';
+            for(var i = 1;i < CenterItems.length;i++){
+                CenterItems[i].style.display = 'block';
+            }
+        }
+        this.parentElement.remove()
+    }else if(this.flag == 1){
+        this.style.backgroundColor = 'red';
+        StudyshowPassageDelItemList.push(StudyshowPassageItemList[this.index]);
+        this.Delindex = StudyshowPassageDelItemList.length - 1; // 保存索引
+        this.flag = 2;
+    }else{
+        this.style.backgroundColor = '';
+        StudyshowPassageDelItemList.splice(this.Delindex,1);
+        this.Delindex = -1;
+        this.flag = 1;
+    }
+}
+
+
+// 批量删除按钮
+StudyshowPassageDelSomeBtn.onclick = function(){
+    StudyshowPassageDelFlag = 1;
+    if(!StudyshowPassageDelSomeFlag){
+        for(var i = 0; i < StudyshowPassageItemList.length; i++){
+            if(StudyshowPassageItemList[i].viewContent){
+                StudyshowPassageItemList[i].btn.className= "StudyshowPassageObjDel";
+                StudyshowPassageItemList[i].btn.flag = 1;
+            }
+        }
+        StudyshowPassageDelSomeFlag = 1;
+    }else{
+        if(StudyshowPassageArea){
+            StudyshowPassageArea.display = 'none';
+            for(var i = 1;i < CenterItems.length;i++){
+                CenterItems[i].style.display = 'block';
+            }
+        }
+        for(var i = 0;i< StudyshowPassageDelItemList.length;i++){
+            StudyshowPassageDelItemList[i].viewContent.remove();
+        }
+        for(var i = 0; i < StudyshowPassageItemList.length; i++){
+            if(StudyshowPassageItemList[i].viewContent){
+                StudyshowPassageItemList[i].btn.className= "iconfont icon-shanchu";
+                StudyshowPassageItemList[i].btn.flag = 0;
+            }
+        }
+        StudyshowPassageDelSomeFlag = 0;
+    }
+}
+
+// 排序按钮功能
+StudyshowPassageSortBtn.onclick = function(){
+    StudyshowPassageList.innerHTML='<div class="iconfont icon-piliangshanchu">批量删除</div>';
+    if(!StudyshowPassageSortFlag){
+        for(var i = StudyshowPassageItemList.length - 1; i >= 0;i--){
+            if(StudyshowPassageItemList[i].viewContent){
+                StudyshowPassageList.appendChild(StudyshowPassageItemList[i].viewContent);
+            }
+        }
+        StudyshowPassageSortFlag = 1;
+    }else{
+        for(var i = 0; i < StudyshowPassageItemList.length;i++){
+            if(StudyshowPassageItemList[i].viewContent){
+                StudyshowPassageList.appendChild(StudyshowPassageItemList[i].viewContent);
+            }
+        }
+        StudyshowPassageSortFlag = 0;
+    }
+    
 }
